9519801737ca9a6e6383ca5c7380d9c803dd3f4d
José Reis R Santiago <josereis@infoway-pi.com.br>
2022-03-05 17:53:19 -0300

    Adding bootstrap template settings.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap";
+
+require.context('../images', true);
+import "./stylesheets/application";
+import "@fortawesome/fontawesome-free/js/all";
+import "@themesberg/volt-bootstrap-5-dashboard/src/assets/js/volt";
 
 Rails.start()
 Turbolinks.start()
