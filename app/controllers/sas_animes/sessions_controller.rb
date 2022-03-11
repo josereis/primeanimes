7eea79ab30c4ad97dc08b3cb19ac785a75e2cbe2
José Reis R Santiago <josereis@infoway-pi.com.br>
2022-03-10 22:17:19 -0300

    Adding after sign and sign out in path for.

--- app/controllers/sas_animes/sessions_controller.rb
+++ app/controllers/sas_animes/sessions_controller.rb
@@ -2,4 +2,12 @@ class SasAnimes::SessionsController < Devise::SessionsController
     def after_sign_out_path_for(admin)
         new_admin_session_path
     end
+
+    def after_sign_in_path_for(admin)
+        sas_animes_root_path
+    end
+
+    def after_sign_out_path_for(admin)
+        new_admins_session_path
+    end
 end
